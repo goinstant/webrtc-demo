fc6a7cb2f6cf8391224891ef170a49db77bd126f
cmac1212 <colin@goinstant.com>
2014-01-15 14:02:44 -0400

    leave button

--- static/js/demo.js
+++ static/js/demo.js
@@ -24,15 +24,21 @@ var UserColors = goinstant.widgets.UserColors;
  * @const
  */
 var LEAVE_CLASS = 'leave-button';
+var LEAVE_MODAL_CLASS = 'leave-modal';
+var CHAT_KEY = 'goinstant/widgets/chat/messages';
 
 function Demo(config) {
   this._config = config;
 
   this._room = null;
-  this._isHost = config.host;
+  this._host = config.host;
+
   this._webrtcContainer = config.webrtcContainer;
   this._webrtcList = config.webrtcList;
   this._chatContainer = config.chatContainer;
+
+  this._webRTC = null;
+  this._chat = null;
 }
 
 Demo.prototype.initialize = function(cb) {
@@ -66,6 +72,7 @@ Demo.prototype._goinstantConnect = function(cb) {
     }
 
     self._room = room;
+    window.room = room;
 
     cb();
   });
@@ -76,22 +83,22 @@ Demo.prototype._initializeWidgets = function(cb) {
     room: this._room
   });
 
-  var webRTC = new WebRTC({
+  this._webRTC = new WebRTC({
     room: this._room,
     expandContainer: this._webrtcContainer,
     listContainer: this._webrtcList,
     autoStart: true
   });
 
-  var chat = new Chat({
+  this._chat = new Chat({
     room: this._room,
     container: this._chatContainer
   });
 
   var tasks = [
     _.bind(userColors.choose, userColors),
-    _.bind(webRTC.initialize, webRTC),
-    _.bind(chat.initialize, chat)
+    _.bind(this._webRTC.initialize, this._webRTC),
+    _.bind(this._chat.initialize, this._chat)
   ];
 
   async.series(tasks, function(err) {
@@ -105,28 +112,54 @@ Demo.prototype._initializeWidgets = function(cb) {
 
 Demo.prototype._setupHost = function(cb) {
   var $leaveButton = $('.' + LEAVE_CLASS);
+  var $leaveModal = $('.' + LEAVE_MODAL_CLASS);
 
-  if (this._isHost) {
+  if (this._host === this._room.name) {
     $leaveButton.css('display', 'block');
 
-    this._room.self().key('host').set(true, function(err) {
+    this._room.self().key('host').set(this._host, function(err) {
       if (err) {
         return cb(err);
       }
     });
   }
 
+  var self = this;
+
   $leaveButton.on('click', function() {
-    if (this._isHost) {
-      console.log('remove keys');
+    if (self._host === self._room.name) {
+      self._room.key(CHAT_KEY).remove(function(err) {
+        if (err) {
+          throw err;
+        }
+
+        self._room.leave();
+        $leaveButton.css('display', 'none');
+      });
     }
   });
 
-  this._room.on('leave', function(userObj) {
-    if (userObj.host) {
-      console.log('timeout, host left');
+  var options = {
+    local: true,
+    listener: function(userObj) {
+      if (userObj.host === self._room.name) {
+        var tasks = [
+          _.bind(self._webRTC.destroy, self._webRTC),
+          _.bind(self._chat.destroy, self._chat)
+        ];
+
+        async.parallel(tasks, function(err) {
+          if (err) {
+            throw err;
+          }
+
+          $leaveModal.css('display', 'block');
+        });
+      }
     }
-  });
+  };
+
+  this._room.on('leave', options);
 };
 
 return Demo;
